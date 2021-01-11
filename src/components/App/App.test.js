6cb535e23957c1a32eea76b4cdae7ac1bef16f32
“Lokesh <lokesh.dr.kataria@gmail.com>
2021-01-11 14:08:04 +0530

    test case for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import reducer from '../../state/reducers/reducers';
 import App from './App';
+import { ThemeProvider } from 'styled-components/macro';
+import { theme } from '../../styles/theme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <ThemeProvider theme={theme}>
+      <Provider store={createStore(reducer)}>
+        <App />
+      </Provider>
+    </ThemeProvider>);
+  const headingElement = screen.getByText(/Login/i);
+  expect(headingElement).toBeInTheDocument();
 });
